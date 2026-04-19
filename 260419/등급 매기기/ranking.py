56853f02fefba6304ad8de730e505e7e81aa0e95
dubu5 <165225046+dubu5@users.noreply.github.com>
2026-04-19 17:39:02 +0900

    Title: 등급 매기기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/등급 매기기/ranking.py
+++ 260419/등급 매기기/ranking.py
@@ -1,11 +1,11 @@
 N = int(input())
 
-if N>90:
+if N>=90:
     print('A')
-elif N>80:
+elif N>=80:
     print('B')
-elif N>70:
+elif N>=70:
     print('C')
-elif N>60:
+elif N>=60:
     print('D')
 else: print('F')
